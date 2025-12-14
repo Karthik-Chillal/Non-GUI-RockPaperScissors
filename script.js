f7127c9e0efe36aa1173b6a35906ca2045a56b12
KarthikChillal <chillalkarthik@gmail.com>
2025-12-14 13:35:21 +0530

    commit#2: computer choice solved.

---
+++ script.js
@@ -0,0 +1,19 @@
+function getComputerChoice(){
+  let compChoice=getRandomInteger(3);
+  switch(compChoice){
+    case 0:
+      return "Rock";
+      break;
+    case 1:
+      return "Paper";
+      break;
+    case 2:
+      return "Scissors";
+      break;
+    default:
+      return "Not Valid";
+  }
+}
+function getRandomInteger(max){
+  return Math.floor(Math.random()*max);
+}
